821846c6889360869b4db43c9db7fe5dee91f28c
Johnny Cheng <johnny.cheng7b9@gmail.com>
2015-08-22 22:56:51 -0700

    Initial setup. Outline factor problem with initial test cases.

---
+++ spec/challenger_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/challenger.rb'
+
+describe Challenger do
+  before(:each) do
+    @challenger = Challenger.new
+  end
+
+  describe '#find_factors' do
+    it 'return array of integers that are factors of the source' do
+      
+    end
+    it 'return empty array when none of the integers in the array is factor of the source' do
+      
+    end
+  end
+
+end
