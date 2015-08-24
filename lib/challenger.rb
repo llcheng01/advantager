b2e0ff4117b674cff6aa7e2aa39d640a6cef5682
Johnny Cheng <johnny.cheng7b9@gmail.com>
2015-08-23 20:34:40 -0700

    Implemented substring method. Able to calculate factors and multipliers for an array of input. Cruel implementation of storing resulting hash in yaml file.

--- lib/challenger.rb
+++ lib/challenger.rb
@@ -1,4 +1,48 @@
+require 'yaml'
+
 class Challenger
+  # data is a hash object
+  # { 10: [], 5: []}
+  attr_accessor :input, :data, :cache_file
+
+  def initialize input = [], cache_file = false
+    @input = input
+    @cache_file = cache_file
+    # @data = cache_file ? YAML::load(File.read(cache_file)) : {}
+  end
+
+  # Set data after all source is calculated
+  def calculate_factors
+    # created a copy of source. sad face!
+    original = @input.dup
+    @input.inject({}) do |data, elem|
+      target = substring elem, original
+      result = factors elem, target, []
+      data[elem] = result
+      data
+    end
+  end
+  
+  def calculate_multipliers
+    # created a copy of source. sad face!
+    original = @input.dup
+    @input.inject({}) do |data, elem|
+      target = substring elem, original
+      result = multipliers elem, target, []
+      data[elem] = result
+      data
+    end
+  end
+
+  # return target array without the source element
+  def substring source, target
+    i = target.index(source)
+    len = target.length
+    return [] if (len == 0 || len == 1)
+    return target[1..len] if i == 0
+    return target[0..len-2] if i == len - 1
+    target[0..i-1] + target[i+1..len-1]
+  end
   # Providing an integer and a list of integer
   # push factor for integer in result
   # result is empty initially
@@ -21,6 +65,13 @@ class Challenger
     result << x if (x % source).zero?
 
     return multipliers source, target, result
+  end
+
+  def save_to_yaml data
+    # @cache_file = cache_file || "cache.yml"
+    File.open @cache_file, "w" do |f|
+      f.write YAML::dump data
+    end
   end
 
 end
--- spec/challenger_spec.rb
+++ spec/challenger_spec.rb
@@ -2,7 +2,39 @@ require 'spec_helper'
 
 describe Challenger do
   before(:each) do
-    @challenger = Challenger.new
+    @challenger = Challenger.new [10, 5, 2, 20], "cache.yml"
+  end
+
+  describe '#substring' do
+    it 'return empty array for array size 0 or 1' do
+      expect(@challenger.substring(50, []).size).to eq 0
+      expect(@challenger.substring(50, [50]).size).to eq 0
+    end
+
+    it 'return correct array for array size is 2' do
+      r = @challenger.substring(50, [50, 10])
+      expect(r.size).to eq 1
+      expect(r).to eq [10]
+      
+    end
+
+    it 'return array without the first element' do
+      r = @challenger.substring(50, [50, 2, 10])
+      expect(r.size).to eq 2
+      expect(r).to eq [2,10]
+    end
+
+    it 'return array without the last element' do
+      r = @challenger.substring(10, [50, 2, 10])
+      expect(r.size).to eq 2
+      expect(r).to eq [50,2]
+    end
+
+    it 'return array without source element' do
+      r = @challenger.substring(2, [50, 2, 10])
+      expect(r.size).to eq 2
+      expect(r).to eq [50,10]
+    end
   end
 
   describe '#factors' do
@@ -24,6 +56,27 @@ describe Challenger do
 
     it 'return empty array when none of the integers in the array is multiplier of the source' do
       expect(@challenger.multipliers(3, [10, 5, 20], [])).to eq []
+    end
+  end
+
+  describe '#calculate_factors' do
+    it 'should return hash of input with factors as array for each input' do
+      result = {10=>[2,5], 5=>[], 2=> [], 20=>[2,5,10]}
+      expect(@challenger.calculate_factors).to eq result
+    end
+  end
+
+  describe '#calculate_multipliers' do
+    it 'should return hash of input with multipliers as array for each input' do
+      result = {10=>[20], 5=>[20,10], 2=>[20,10], 20=>[]}
+      expect(@challenger.calculate_multipliers).to eq result
+    end
+  end
+
+  describe '#save_to_yaml' do
+    it 'should be able to save result to yaml file' do
+      data = @challenger.calculate_factors
+      @challenger.save_to_yaml data
     end
   end
 
